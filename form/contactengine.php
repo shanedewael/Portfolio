5e437a4d6b526964ab63c7f88b89f8459c73665b
Shane DeWael <me@shanedewael.com>
2014-11-04 10:52:46 -0500

    Original portfolio commit

---
+++ form/contactengine.php
@@ -0,0 +1,41 @@
+                                                                <?php
+
+$EmailFrom = "helloworld@purdue.edu";
+$EmailTo = "me@shanedewael.com";
+$Subject = "Contact Form";
+$Name = Trim(stripslashes($_POST['Name'])); 
+$Email = Trim(stripslashes($_POST['Email'])); 
+$Message = Trim(stripslashes($_POST['Message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=www.shanedewael.com">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>
+                            
+                            
